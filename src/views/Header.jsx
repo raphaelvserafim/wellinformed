41b0bb08a3f709c2a11dcb49286626b49233476a
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-13 23:27:40 -0400

    feat: menu

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -7,27 +7,20 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import TemporaryDrawer from './Menu';
 
 export default function Header() {
-    return (
-        <Box sx={{ flexGrow: 1, position: 'fixed', top: 0, width: '100%', zIndex: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1, position: 'fixed', top: 0, width: '100%', zIndex: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <TemporaryDrawer />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
